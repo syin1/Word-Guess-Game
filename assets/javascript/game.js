b2617d0acff8088d0dc1bbe7b068c43a9af8889a
syin <shaomingyinut@gmail.com>
2018-06-22 21:57:55 -0400

    All basic functions implemented

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   var answer = 'great';
   var remaining = 12;
+  var lettersguessed = [];
+  var gameswon = 0;
+  var gameslost = 0;
 
-  $('#random-button').on('click', function() {
+  function restart() {
     // Create a string which will hold the lottery number
     var lottoNumber = '';
 
@@ -20,12 +23,20 @@ $(document).ready(function() {
     // ... and then dump the random number into our random-number div.
     // $('#random-number').prepend('<br><hr>' + lottoNumber);
     $('#random-number').html(lottoNumber);
-  });
+  }
+
+  restart();
+
+  // $('#random-button').on('click', function() {
+  //   restart();
+  // });
 
   $(document).keypress(function(event) {
     var guess = '';
     var guesswrong = 0;
+    var guessright = 0;
 
+    // get current word
     var current = $('#random-number')
       .text()
       .replace(/\s/g, '');
@@ -35,8 +46,10 @@ $(document).ready(function() {
 
       if (event.key === answer[i]) {
         letter = answer[i] + ' ';
+        guessright++;
       } else if (current[i] !== '_') {
         letter = current[i] + ' ';
+        guessright++;
         guesswrong++;
       } else {
         letter = '_ ';
@@ -46,11 +59,38 @@ $(document).ready(function() {
       guess = guess + letter;
     }
 
-    if (guesswrong === answer.length) {
+    if (
+      guesswrong === answer.length &&
+      $.inArray(event.key, lettersguessed) === -1
+    ) {
       remaining--;
+      lettersguessed.push(event.key);
     }
 
     $('#random-number').html(guess);
     $('#remaining').html(remaining);
+    $('#lettersguessed').html(lettersguessed.join(', '));
+
+    if (guessright === answer.length) {
+      gameswon++;
+      $('#gameswon').html(gameswon);
+      restart();
+      remaining = 12;
+      $('#remaining').html(remaining);
+
+      lettersguessed = [];
+      $('#lettersguessed').html(lettersguessed.join(' '));
+    }
+
+    if (remaining === 0) {
+      gameslost++;
+      $('#gameslost').html(gameslost);
+      restart();
+      remaining = 12;
+      $('#remaining').html(remaining);
+
+      lettersguessed = [];
+      $('#lettersguessed').html(lettersguessed.join(' '));
+    }
   });
 });
