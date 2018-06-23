c2923d30ed85b6fc80a8450efe7287ca0aece0fc
syin <shaomingyinut@gmail.com>
2018-06-23 10:13:25 -0400

    Clean up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,30 +6,17 @@ $(document).ready(function() {
   var gameslost = 0;
 
   function restart() {
-    // Create a string which will hold the lottery number
-    var lottoNumber = '';
+    var setup = '';
 
-    // Then initiate a loop to generate 9 separate numbers
     for (var i = 0; i < answer.length; i++) {
-      // For each iteration, generate a new random number between 1 and 9.
-      // var random = Math.floor(Math.random() * 9) + 1;
-      var random = '_ ';
-
-      // Take this number and then add it to the rest of the string.
-      // In essence, we are iteratively building a string of numbers. (e.g. First: 1, Second: 13, Third: 135, etc.)
-      lottoNumber = random + lottoNumber;
+      var letter = '_ ';
+      setup = letter + setup;
     }
 
-    // ... and then dump the random number into our random-number div.
-    // $('#random-number').prepend('<br><hr>' + lottoNumber);
-    $('#random-number').html(lottoNumber);
+    $('#random-number').html(setup);
   }
 
   restart();
-
-  // $('#random-button').on('click', function() {
-  //   restart();
-  // });
 
   $(document).keypress(function(event) {
     var guess = '';
